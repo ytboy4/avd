8bf23f8b0bc361582d55d36d11b845ecf0a55e94
ytboy4 <adamwhite78@googlemail.com>
2025-12-01 16:06:17 +0000

    Update sosinstall.ps1

--- sosinstall.ps1
+++ sosinstall.ps1
@@ -1,2 +1,2 @@
-#Start-Process '\\vm-dpg-app-1\sos\Install\SOS Install.bat'
-Start-Process cmd -ArgumentList '"\\vm-dpg-app-1\sos\Install\SOS Install.bat"'
+Start-Process '\\vm-dpg-app-1\sos\Install\SOS Install.bat'
+
